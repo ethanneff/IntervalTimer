e2fb773b0d17c5c1f131a702bf78093fda61be2e
Ethan Neff <eneff@Ethans-MacBook-Pro.local>
2014-09-04 09:21:39 -0500

    Initial Commit

---
+++ Interval Timer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Interval Timer
+//
+//  Created by Ethan Neff on 9/4/14.
+//  Copyright (c) 2014 ethanneff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Interval TimerTests/Interval_TimerTests.m
@@ -0,0 +1,34 @@
+//
+//  Interval_TimerTests.m
+//  Interval TimerTests
+//
+//  Created by Ethan Neff on 9/4/14.
+//  Copyright (c) 2014 ethanneff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Interval_TimerTests : XCTestCase
+
+@end
+
+@implementation Interval_TimerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
